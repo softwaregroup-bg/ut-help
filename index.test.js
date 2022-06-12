3d6746f11f016b699295a5b9bcfdde6c570cbf46
Kalin Krustev <kalin.krustev@gmail.com>
2022-06-13 00:25:22 +0300

    test: fix tests

--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
 const tap = require('tap');
 
 tap.test('require', async(assert) => {
-    const help = require('.')({include: ['.']});
+    const help = require('.');
     assert.ok(help, 'require');
     const sidebar = await help.presets[0][1].docs.sidebarItemsGenerator({
         defaultSidebarItemsGenerator: () => [{items: [{}]}]
---
+++ utHelp.js
@@ -0,0 +1,4 @@
+module.exports = {
+    resolve: require.resolve,
+    include: ['.']
+};
